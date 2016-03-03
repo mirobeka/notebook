95242184b436a04bb76ef4c4c206f85fb81b15bd
Miroslav Beka <miroslav.beka@kistler.com>
2016-03-03 11:16:21 +0100

    Fixes url routing with ending slash
    modified:   notebook/api.py

--- notebook/api.py
+++ notebook/api.py
@@ -10,29 +10,29 @@ def index():
     # return api discovery string
     return "You've discovered notebook api!"
 
-@notebook_api.route("/notes", methods=["GET"])
+@notebook_api.route("/notes/", methods=["GET"])
 def get_notes():
     notes = Note.get_sample_notes()
     return jsonify(notes)
 
-@notebook_api.route("/notes", methods=["POST"])
+@notebook_api.route("/notes/", methods=["POST"])
 def post_product_builds():
     data = request.json['data']
     # create new note
     note = {'id': 5, 'data': data}
     return jsonify(note)
 
-@notebook_api.route("/notes/<int:note_id>", methods=["GET"])
+@notebook_api.route("/notes/<int:note_id>/", methods=["GET"])
 def get_product_build(note_id):
     note = {'id':note_id, 'data': 'this is note with id {}'.format(note_id)}
     return jsonify(note)
 
-@notebook_api.route("/notes/<int:note_id>", methods=["PUT"])
+@notebook_api.route("/notes/<int:note_id>/", methods=["PUT"])
 def put_product_build(note_id):
     note = request.json
     return jsonify(note)
 
-@notebook_api.route("/notes/<int:note_id>", methods=["DELETE"])
+@notebook_api.route("/notes/<int:note_id>/", methods=["DELETE"])
 def delete_product_build(note_id):
     return jsonify({"result" : True})
 
